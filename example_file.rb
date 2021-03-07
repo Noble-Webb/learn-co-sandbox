8e599d8b10e7a34cbc27b251f9dbea5b49d37986
Justin Webb <webb.n.justin@gmail.com>
2021-03-07 20:46:31 +0000

    Automatically backed up by Learn

---
+++ example_file.rb
@@ -0,0 +1,7 @@
+greeting = "unfriendly_greeting"
+case greeting
+  when  "unfriendly_greeting"
+    print "Rude"
+  when "friendly_greeting"
+    print "Thank you"
+end
---
+++ learning-comments.rb
@@ -0,0 +1,12 @@
+array.each do |message|
+  puts "#{message} loves you."
+end 
+
+def square_array(numbers)
+  
+  new_array = Math.exp([])
+  
+  while numbers.length [new_array] do 
+  puts new_array
+  end
+end
